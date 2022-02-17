25c9cba912e7e0b33f65a9a4f4693211c017f2ee
LAB1INF-03\Administrador <spruna94@gmail.com>
2022-02-17 20:06:07 +0100

    tresEnRatlla

--- exercicis/src/App.js
+++ exercicis/src/App.js
@@ -13,6 +13,7 @@ function App() {
       <Acordio />
        <Carrusel />
       */}
+
       <TresEnRatlla />
     </div>
   );
--- exercicis/src/TresEnRatlla/TresEnRatlla.js
+++ exercicis/src/TresEnRatlla/TresEnRatlla.js
@@ -2,9 +2,7 @@ import { useState } from "react";
 import "./TresEnRatlla.css";
 
 function TresEnRatlla() {
-  let array = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
-
-  const [print, setPrint] = useState([
+  const [array, setArray] = useState([
     "-",
     "-",
     "-",
@@ -16,13 +14,67 @@ function TresEnRatlla() {
     "-",
   ]);
 
-  const ChangeValue = (index) => {
-    let copia = [...array];
+  const [value, setValue] = useState(true);
+  const [win, setWin] = useState(false);
+  const [texto, setTexto] = useState(null);
 
-    if (print === "-") {
-      copia[index] = "X";
-      setPrint(copia[index]);
-    } else {
+  let copia = [...array];
+
+  const ChangeValue = (i) => {
+    if (array.includes("-") === false) {
+      setTexto("EMPATE");
+      setWin(true);
+    }
+    if (copia[i] === "-" && value === true) {
+      copia[i] = "X";
+      setArray(copia);
+      setValue(false);
+    } else if (copia[i] === "-" && value === false) {
+      copia[i] = "O";
+      setArray(copia);
+      setValue(true);
+    }
+    checkEnd();
+    Reinico();
+  };
+  const Reinico = () => {
+    if (win === true) {
+      document.location.reload(true);
+    }
+  };
+
+  const checkEnd = () => {
+    if (copia[0] === copia[1] && copia[1] === copia[2] && copia[0] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[0] + " han ganado");
+    }
+    if (copia[3] === copia[4] && copia[4] === copia[5] && copia[3] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[3] + " han ganado");
+    }
+    if (copia[6] === copia[7] && copia[7] === copia[8] && copia[6] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[6] + " han ganado");
+    }
+    if (copia[0] === copia[3] && copia[3] === copia[6] && copia[0] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[0] + " han ganado");
+    }
+    if (copia[1] === copia[4] && copia[4] === copia[7] && copia[1] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[1] + " han ganado");
+    }
+    if (copia[2] === copia[5] && copia[5] === copia[8] && copia[2] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[2] + " han ganado");
+    }
+    if (copia[0] === copia[4] && copia[4] === copia[8] && copia[0] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[0] + " han ganado");
+    }
+    if (copia[2] === copia[4] && copia[4] === copia[6] && copia[2] !== "-") {
+      setWin(true);
+      setTexto("Las " + copia[2] + " han ganado");
     }
   };
 
@@ -31,11 +83,12 @@ function TresEnRatlla() {
       <div className="DisplayTres">
         <div className="BotonsBox">
           {array.map((boto, i) => (
-            <button className="Boto" onClick={ChangeValue}>
-              {boto}
+            <button className="boto" onClick={() => ChangeValue(i)}>
+              {array[i]}
             </button>
           ))}
         </div>
+        <h1>{texto}</h1>
       </div>
     </div>
   );
